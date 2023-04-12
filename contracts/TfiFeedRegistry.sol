d3c7f22011c87c1314f4711e422edea104be312c
Joseph C Wang <joequant@gmail.com>
2023-04-12 11:50:49 +0800

    add access control

--- contracts/TfiFeedRegistry.sol
+++ contracts/TfiFeedRegistry.sol
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract TfiFeedRegistry is Initializable, OwnableUpgradeable {
+contract TfiFeedRegistry is Initializable, OwnableUpgradeable,
+AccessControlUpgradeable {
   struct RoundData {
     int256 answer;
     uint256 startedAt;
     uint256 updatedAt;
   }
   uint80 public latestRound;
+  mapping (string => bool) private global;
   mapping (string => mapping(uint80 => RoundData)) private data;
 
   function initialize(
   ) public initializer {
     __Ownable_init();
+    __AccessControl_init();
   }
 
   function getRoundData(string memory key, uint80 roundId)
@@ -30,6 +35,9 @@ contract TfiFeedRegistry is Initializable, OwnableUpgradeable {
     uint80 answeredInRound
   )
   {
+    require(
+      global[key] || hasRole(roleId("read", key), msg.sender)
+    );
     RoundData memory rd = data[key][roundId];
     return (roundId, rd.answer, rd.startedAt, rd.updatedAt, latestRound);
   }
@@ -59,8 +67,22 @@ contract TfiFeedRegistry is Initializable, OwnableUpgradeable {
   external
   virtual
   {
+    require(hasRole(roleId("set", key), msg.sender));
     latestRound = roundId;
     data[key][roundId] = RoundData(answer, startedAt, updatedAt);
   }
 
+  function roleId(
+    string memory role,
+    string memory key
+  ) public pure returns (bytes32) {
+    return keccak256(abi.encodePacked(role, key));
+  }
+
+  function setGlobal(
+    string memory key_,
+    bool value_
+  ) public {
+    global[key_] = value_;
+  }
 }
